712b424628d23be0d7e6a4d808d992d2cd3bbe45
OriginalSoymilk <16samuelko@gmail.com>
2025-05-26 18:33:29 +0800

    dispair

--- query_faiss.py
+++ query_faiss.py
@@ -14,12 +14,18 @@ def load_resources():
     if model is None:
         model = SentenceTransformer('all-MiniLM-L6-v2')
     if index is None:
+        print("Loading faiss...")
         index = faiss.read_index("nutrition_index.faiss")
+        print("faiss success")
     if documents is None:
+        print("Loading pkl")
         with open("nutrition_docs.pkl", "rb") as f:
             documents = pickle.load(f)
+        print("pkl success")
 
 def search_similar_documents(query: str, top_k: int = 5) -> str:
+    print("查詢 query：", query)
+    print("找到文件：", relevant_docs)
     load_resources()
     embedding = model.encode([query])
     D, I = index.search(np.array(embedding), k=top_k)
